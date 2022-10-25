18cd63fcf8c9fc48562c8149911b8e966c5df5ee
aurxf <aurxfpro@gmail.com>
2022-10-25 05:55:30 +0200

    42

--- 42Cursus/libft/libft.h
+++ 42Cursus/libft/libft.h
@@ -3,16 +3,16 @@
 
 #include <stddef.h>
 
-int ft_atoi(const char *theString)
-int ft_isalnum(int character)
-int ft_isalpha(int character)
-int ft_isascii(int character)
-int ft_isdigit(int character)
-int ft_isprint(int character)
-char    *ft_strchr(const char *string, int searchedChar)
-char    *ft_strrchr(const char *string, int searchedChar)
-size_t  ft_strlen(const char *theString)
-int ft_tolower(int character)
-int ft_toupper(int character)
+int ft_atoi(const char *theString);
+int ft_isalnum(int character);
+int ft_isalpha(int character);
+int ft_isascii(int character);
+int ft_isdigit(int character);
+int ft_isprint(int character);
+char    *ft_strchr(const char *string, int searchedChar);
+char    *ft_strrchr(const char *string, int searchedChar);
+size_t  ft_strlen(const char *theString);
+int ft_tolower(int character);
+int ft_toupper(int character);
 
 #endif
