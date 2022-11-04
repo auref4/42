af2905758d8518590535667f54c21939253f32b5
aurxf <aurxfpro@gmail.com>
2022-11-04 13:36:16 +0100

    libft

--- 42Cursus/libft/ft_strncmp.c
+++ 42Cursus/libft/ft_strncmp.c
@@ -5,7 +5,7 @@ int ft_strncmp(const char *first, const char *second, size_t lenght)
     size_t  i;
 
     i = 0;
-    if(lenght == 0)
+    if (lenght == 0)
         return (0);
     while (first[i] && second[i] && first[i] == second[i] && i < lenght)
         i++;
