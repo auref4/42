bac151fc7f4b4e5a83241ff7b83539a95ad827fd
Aurelien Ferrand <auferran@e2r3p16.clusters.42paris.fr>
2022-11-14 20:45:42 +0100

    libft

--- 42Cursus/libft/libft.h
+++ 42Cursus/libft/libft.h
@@ -36,5 +36,6 @@ void	ft_putendl_fd(char *s, int fd);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 
 #endif
