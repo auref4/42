f05150cb1eb0a69a746108a67e8659e2e3a7e4e0
Aurelien FERRAND <auferran@e1r2p22.clusters.42paris.fr>
2022-11-25 14:11:01 +0100

    42

--- 42Cursus/ft_printf/ft_printf.c
+++ 42Cursus/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: auferran <auferran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 16:59:20 by auferran          #+#    #+#             */
-/*   Updated: 2022/11/24 18:52:51 by auferran         ###   ########.fr       */
+/*   Updated: 2022/11/25 12:56:58 by auferran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,14 @@ int	ft_printf(const char *format, ...)
 	while (format[i])
 	{
 		if (format[i] == '%' && ft_check_pc_letter(format[i + 1]))
-			len += ft_print_type(args, format[i++ + 1]);
-		else
 		{
-			len += ft_print_char(format[i]);
-			if (len == -1)
-				return (-1);
+			len += ft_print_type(args, format[i + 1]);
+			i++;
 		}
+		else
+			len += ft_print_char(format[i]);
+		if (len == -1)
+			return (-1);
 		i++;
 	}
 	va_end(args);
