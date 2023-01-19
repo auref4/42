5814645544271adb64a27e02c245f5e27862e09c
Aurelien FERRAND <auferran@e1r2p13.clusters.42paris.fr>
2023-01-19 01:59:28 +0100

    42

--- 42Cursus/push_swap/ft_push_swap.c
+++ 42Cursus/push_swap/ft_push_swap.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_push_swap.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: auferran <auferran@student.42.fr>          +#+  +:+       +#+        */
+/*   By: auferran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/17 23:35:46 by auferran          #+#    #+#             */
-/*   Updated: 2023/01/19 01:47:33 by auferran         ###   ########.fr       */
+/*   Created: 2023/01/19 00:49:54 by auferran          #+#    #+#             */
+/*   Updated: 2023/01/19 01:09:23 by auferran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+int	ft_push_swap(char **argv)
 {
-	int	i;
 
-	i = 1;
-	if (argc != 2)
-	{
-		while (argv[i])
-		{
-			ft_atoi(argv[i]);
-			i++;
-		}
-		ft_push_swap(argv);
-	}
-	return (0);
 }
--- 42Cursus/push_swap/push_swap.h
+++ 42Cursus/push_swap/push_swap.h
@@ -6,12 +6,22 @@
 /*   By: auferran <auferran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 23:31:40 by auferran          #+#    #+#             */
-/*   Updated: 2023/01/17 23:49:12 by auferran         ###   ########.fr       */
+/*   Updated: 2023/01/19 01:29:41 by auferran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-
+//typedef struct Element;
+//{
+//	int	nb;
+//	Element	*next;
+//	Element	*prev;
+//}	element;
+int	ft_atoi(const char *s);
+//void	push(element **element_a, element **element_b);
+//void	swap(element **element_b, element **element_b);
+//int	element_size(element *element_a);
+//int	*ft_push_swap(char *argv);
 # endif
