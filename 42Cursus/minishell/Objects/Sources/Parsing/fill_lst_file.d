650df9f232ed1f5d23e2df4fdd709da9b730f846
aurxf <aurxfpro@gmail.com>
2023-11-27 07:39:48 +0100

    42

--- 42Cursus/minishell/Objects/Sources/Parsing/fill_lst_file.d
+++
@@ -1,2 +0,0 @@
-Objects/./Sources/Parsing/fill_lst_file.o: \
- Sources/Parsing/fill_lst_file.c Includes/minishell.h
--- 42Cursus/minishell/Objects/Sources/Parsing/manage_dollar_2.d
+++
@@ -1,2 +0,0 @@
-Objects/./Sources/Parsing/manage_dollar_2.o: \
- Sources/Parsing/manage_dollar_2.c Includes/minishell.h
