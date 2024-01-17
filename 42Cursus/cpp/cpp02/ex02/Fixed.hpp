bdb5294b97bc4b398a27b5d15c8cfec09b6d7370
aurxf <aurxfpro@gmail.com>
2024-01-17 10:45:40 +0100

    cpp

---
+++ 42Cursus/cpp/cpp02/ex02/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef	FIXED_HPP
+#define	FIXED_HPP
+
+#include <iostream>
+
+class	Fixed
+{
+	public :
+
+	Fixed();
+	Fixed(int nb);
+	Fixed(Fixed const &src);
+	~Fixed();
+
+	Fixed&	operator=(Fixed const &rhs);
+	Fixed	operator+(Fixed const &rhs);
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+
+	private :
+
+	int					m_nb;
+	int const static	fract = 8;
+};
+
+#endif
