e0ee58a39d71673e34ef275a51139738b815641c
aurxf <aurxfpro@gmail.com>
2024-06-17 22:48:32 +0200

    cpp08

---
+++ 42Cursus/cpp/cpp08/ex01/Span.hpp
@@ -0,0 +1,41 @@
+#ifndef	SPAN_HPP
+#define	SPAN_HPP
+
+#include <iostream>
+#include <exception>
+
+class	Span
+{
+	public :
+
+	Span();									//CANONICAL
+	Span(unsigned int n);
+	Span(Span const &src);					//CANONICAL
+	~Span();								//CANONICAL
+
+	Span&	operator=(Span const &rhs);		//CANONICAL
+
+	void	addNumber(int nb);
+	void	addMultipleNumbers();
+	int		shortestSpan(void) const;
+	int		largestSpan(void) const;
+
+	class	FullElements : public std::exception
+	{
+		public :
+
+		virtual const char*	what() const throw();
+	}
+	class	VoidElements : public std::exception
+	{
+		public :
+
+		virtual const char*	what() const throw();
+	}
+
+	private :
+
+	int	*elements;
+};
+
+#endif
