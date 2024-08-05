0bbd80efcb0cccc2845cb8cf658e9e6439b1e697
aurxf <aurxfpro@gmail.com>
2024-08-06 00:19:52 +0200

    cpp08 finished

--- 42Cursus/cpp/cpp08/ex02/MutantStack.hpp
+++ 42Cursus/cpp/cpp08/ex02/MutantStack.hpp
@@ -2,16 +2,31 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
+#include <iostream>
 
-template<typename T>
-class MutantStack : public stack
+template<typename T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
 {
 	public :
 
-	MutantStack();											//CANONICAL
-	MutantStack(MutantStack const &src);					//CANONICAL
-	~MutantStack();											//CANONICAL
-	MutantStack&	operator=(MutantStack const &rhs);		//CANONICAL
-}
+	MutantStack() : std::stack<T, Container>() {};								//CANONICAL
+	MutantStack(MutantStack const &src) : std::stack<T, Container>(src) {};		//CANONICAL
+	~MutantStack() {};															//CANONICAL
+	MutantStack&	operator=(MutantStack const &rhs)							//CANONICAL
+	{
+		if (this != &rhs)
+			std::stack<T, Container>::operator=(rhs);
+		return *this;
+	};
+	typedef typename std::stack<T, Container>::container_type::iterator iterator;
+	iterator	begin()
+	{
+		return std::stack<T, Container>::c.begin();
+	};
+	iterator	end()
+	{
+		return std::stack<T, Container>::c.end();
+	};
+};
 
 #endif
