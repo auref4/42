1bc07fb09dd54d6bb04b020a30d24b538ab65a92
aurxf <auref.contact@protonmail.com>
2025-05-05 22:47:03 +0200

    inception

--- 42Cursus/inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ 42Cursus/inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Vérifie si le répertoire de base de données existe
+#init database
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initialisation de MariaDB..."
-    mysqld --initialize-insecure --user=mysql  # Initialise la base de données si elle n'existe pas
+    mysqld --initialize-insecure --user=mysql
 fi
 
-# Lance MariaDB en mode sécurisé
+#exec mariadb
 exec mysqld_safe --datadir='/var/lib/mysql' --user=mysql
-
